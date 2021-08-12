bd66015170b4ef058eca8c4876bb5cf539b857fe
Isaac <312764843@qq.com>
2021-08-12 21:58:49 +0800

    :q Xpage

--- src/main/java/com/westone/lijingjing/controller/UserController.java
+++ src/main/java/com/westone/lijingjing/controller/UserController.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -20,7 +21,7 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-    @RequestMapping("/login")
+    @RequestMapping(value = "/api/login", method = RequestMethod.POST)
     public HTTPResponse<User> login(@RequestBody UserLoginRequest userLoginRequest) {
         HTTPResponse<User> response = new HTTPResponse<>();
         logger.info("user try to login|request={}", userLoginRequest);
